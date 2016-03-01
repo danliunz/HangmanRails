7fe41092cc1e3ed66b8c42cffde7b03e0b4f7340
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-03-02 12:58:01 +1300

    remove whitespace

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,6 +2,7 @@ class Guess < ActiveRecord::Base
   belongs_to :game
   
   validates :game, presence: true
+  
   validates :letter, format: {
     with: /\A[[:lower:]]\z/,
     message: "must be single lower-case alphabetic letter"

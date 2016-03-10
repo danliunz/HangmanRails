e1723d0acaeda15e958b4b665d9ad536453224a9
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-03-10 16:57:21 +1300

    code refactoring

--- app/presenters/game_presenter.rb
+++ app/presenters/game_presenter.rb
@@ -1,22 +1,32 @@
-class GamePresenter < SimpleDelegator
+class GamePresenter
   LAST_PLAY_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
   OBSCURED_SECRET_LETTER = " _ "
 
+  delegate :won?, :lost?, :secret, to: :@game
+
+  def initialize(game)
+    @game = game
+  end
+
+  def delegate
+    @game
+  end
+
   def visible_to_player?(secret_letter)
-    over? || guessed?(secret_letter)
+    @game.over? || @game.guessed?(secret_letter)
   end
 
   def obscured_secret
-    secret.chars.map do |letter|
+    @game.secret.chars.map do |letter|
       visible_to_player?(letter) ? letter : OBSCURED_SECRET_LETTER
     end.join
   end
 
   def guesses_as_string
-    guesses.order(id: :asc).map(&:letter).join(" ")
+    @game.guesses.order(id: :asc).map(&:letter).join(" ")
   end
 
   def last_played_at
-    updated_at.strftime(LAST_PLAY_TIME_FORMAT)
+    @game.updated_at.strftime(LAST_PLAY_TIME_FORMAT)
   end
 end
